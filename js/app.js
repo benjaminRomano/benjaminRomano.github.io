e7bc1f98053a242c7c83559583f5b5337fb334d7
bromano <benjamin.romano@outlook.com>
2016-03-09 07:34:08 -0600

    Added working skills section

--- js/app.js
+++ js/app.js
@@ -1,1 +1,28 @@
-$(document).foundation();
+// $(document).foundation();
+
+
+var skillSelectors = {
+    '#languageSelector' : '#languages',
+    '#databaseSelector': '#databases',
+    '#frameworkSelector': '#frameworks',
+    '#gameEngineSelector': '#gameEngines'
+};
+
+var skillClickHandler = function(e) {
+}
+
+$(document).ready(function() {
+    for (let s in skillSelectors) {
+        let ele = $(s);
+        ele.click(function(e) {
+            for (let s2 in skillSelectors) {
+                let otherEle = $(s2);
+                otherEle.removeClass('active');
+                $(skillSelectors[s2]).hide();
+            }
+            
+            ele.addClass('active');
+            $(skillSelectors[s]).show();
+        });
+    }
+});
