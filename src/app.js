849eee3d576f506f859f450b25dbe4d019e3b8a3
Benjamin Romano <benjamin.romano@outlook.com>
2016-03-10 12:04:52 -0600

    Fixed about me header and misspelled names

--- src/app.js
+++ src/app.js
@@ -1,7 +1,7 @@
 let skillGroups = {
     '#languages': ['C#', 'JavaScript', 'Node.js', 'Ruby', 'Python', 'Java', 'C++', 'Perl', 'Go', 'FORTRAN', 'Scheme', 'TypeScript'],
     '#databases': ['SQL Server', 'MySql', 'MongoDb'],
-    '#frameworks': ['Angular.js', 'ASP.NET Web API', 'ASP.NET MVC', 'Ruby on Rails', 'Django', 'MEAN Stack'],
+    '#frameworks': ['AngularJS', 'ASP.NET Web API', 'ASP.NET MVC', 'Ruby on Rails', 'Django', 'MEAN Stack'],
     '#gameEngines': ['Unreal Engine', 'Unity Engine']
 };
 
